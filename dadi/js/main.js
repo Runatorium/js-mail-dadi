c1fb24d0072d5397c5119d6dfdee29c1cd122e68
Runatorium <stefano.alesci1@gmail.com>
2023-02-27 18:07:03 +0100

    pareggio

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -4,6 +4,8 @@ const randomnumber2 = Math.floor(Math.random() * 6) + 1;
 
 if(randomnumber1<randomnumber2){
     console.log('vince il giocatore')
-}else{
+}else if(randomnumber2<randomnumber1) {
     console.log('vince il computer')
+}else{
+    console.log('pareggio')
 }
