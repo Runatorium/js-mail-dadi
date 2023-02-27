597d19575e002bef869d86416ea471ac0d4993ca
Runatorium <stefano.alesci1@gmail.com>
2023-02-27 15:50:09 +0100

    new folders commit

---
+++ dadi/js.dadi/main.js
@@ -0,0 +1,8 @@
+let rngnumbers1 = [];
+let rngnumbers2 = [];
+
+for(i=0; i<5; i++){
+    let randomnumber = Math.floor(Math.random() * 6) + 1;
+    rngnumbers1.push(randomnumber);
+}
+console.log(rngnumbers1)
