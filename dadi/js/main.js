01751d1890118db899e5e2537e85613e22baf4b1
Runatorium <stefano.alesci1@gmail.com>
2023-02-27 16:50:35 +0100

    esercizio dadi rifatto

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,30 +1,9 @@
-let rngnumbers1 = [];
-let rngnumbers2 = [];
-let sum1 = 0;
-let sum2 = 0;
 
+const randomnumber1 = Math.floor(Math.random() * 6) + 1;
+const randomnumber2 = Math.floor(Math.random() * 6) + 1;
 
-for(i=0; i<12; i++){
-    if(i<6){
-        const randomnumber = Math.floor(Math.random() * 6) + 1;
-        rngnumbers1.push(randomnumber);
-        sum1 += rngnumbers1[i]; 
-    }else{
-        const randomnumber = Math.floor(Math.random() * 6) + 1;
-        rngnumbers2.push(randomnumber); 
-        sum2 += rngnumbers2[i-6];
-    }
-}
-console.log(sum1);
-console.log(sum2);
-console.log(rngnumbers1);
-console.log(rngnumbers2);
-
-if(sum1 < sum2){
-    console.log('vince il computer');
-
-}else if(sum1 > sum2){
-    console.log('vince il giocatore');
+if(randomnumber1<randomnumber2){
+    console.log('vince il giocatore')
 }else{
-    console.log('pareggio');
+    console.log('vince il computer')
 }
