40e9fd959189414f9863d812ae33a9de894a33a0
Runatorium <stefano.alesci1@gmail.com>
2023-02-27 15:54:25 +0100

    .

--- dadi/js.dadi/js/main.js
+++ dadi/js.dadi/js/main.js
@@ -2,7 +2,9 @@ let rngnumbers1 = [];
 let rngnumbers2 = [];
 
 for(i=0; i<5; i++){
-    let randomnumber = Math.floor(Math.random() * 6) + 1;
+    const randomnumber = Math.floor(Math.random() * 6) + 1;
+    console.log(randomnumber);
     rngnumbers1.push(randomnumber);
+    
 }
 console.log(rngnumbers1)
